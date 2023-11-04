78ec3c176d1109ab26877bb9abfcd1ba02417a2b
Andrew Wang <awwang3@uci.edu>
2023-11-04 11:55:11 -0700

    path add card set

--- server/api.py
+++ server/api.py
@@ -1,9 +1,7 @@
-from flask import Blueprint, request, jsonify
-import json
-
+from flask import Blueprint, request
 from sqlalchemy.testing import db
 
-from zotcards.server.model import User
+from zotcards.server.model import User, CardSet, Card, AnswerChoice
 
 api = Blueprint("api", __name__)
 
@@ -17,8 +15,33 @@ def index():
 def testdata():
     data = request.json
 
-    email = jsonify(data.get('email'))
+    # GET DATA
+    #
 
-    new_user = User(email=email)
+    new_user = User(email='')  # CHANGE
     db.session.add(new_user)
+    db.session.commit()
+
+
+@api.route("/api/auth/create-card-set", methods=["GET", "POST"])
+def testdata():
+    data = request.json
+
+    # GET DATA
+
+    #
+
+    # CHANGE
+    card_set = CardSet()
+    cards = [Card()]
+    answer_choices = [AnswerChoice()]
+
+    for card in cards:
+        card_set.cards.append(card)
+
+    for answer_choice in answer_choices:
+        card_set.cards.append(answer_choice)
+    #
+
+    db.session.add(card_set)
     db.session.commit()
