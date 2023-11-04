3f73de12a8ace59cbf88b243442d70d8fe6e249d
Andrew Wang <awwang3@uci.edu>
2023-11-04 13:17:32 -0700

    fix imports

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -2,12 +2,17 @@ from flask import Flask
 from flask_cors import CORS
 
 from .api import api
+from .model import db
+
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
 
     app.config["SECRET_KEY"] = "my-secret"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+
+    db.init_app(app)
 
     app.register_blueprint(api)
 
