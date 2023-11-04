de6bd32e83c263f6cf7aeaa2b56ed50cd497d2d7
Dalton Adcock <dadcock@uci.edu>
2023-11-04 15:32:02 -0700

    database populator

--- server/app/api.py
+++ server/app/api.py
@@ -3,6 +3,7 @@ import openai
 from dotenv import load_dotenv
 from flask import Blueprint, request
 from sqlalchemy.testing import db
+import json
 
 from .model import User, CardSet, Card, AnswerChoice
 
@@ -26,25 +27,26 @@ def signup():
     db.session.commit()
 
 
+def gpt_string_to_array(string):
+    string = string.replace("'", '"')
+    result = list(json.loads(string).values())
+    return result
+
+
 @api.route("/api/auth/create-card-set", methods=["GET", "POST"])
-def create_card_set():
-    data = request.json
-
-    # GET DATA
-
-    #
-
-    # CHANGE
+def create_card_set(parsed_set):
+    
     card_set = CardSet()
-    cards = [Card()]
-    answer_choices = [AnswerChoice()]
+    cards = []
 
-    for card in cards:
-        card_set.cards.append(card)
-
-    for answer_choice in answer_choices:
-        card_set.cards.append(answer_choice)
-    #
+    for card in parsed_set:
+        answer_choices = []
+        for answer in card["choices"]:
+            if answer == card["correct"]:
+                answer_choices.append(AnswerChoice(answer_text=card["choices"]["answer"], is_correct=True))
+            else:
+                answer_choices.append(AnswerChoice(answer_text=card["choices"]["answer"], is_correct=True))
+        cards.append(Card(question_text=card, answer_choices=answer_choices))
 
     db.session.add(card_set)
     db.session.commit()
@@ -82,7 +84,6 @@ def get_question():
 
     notes = data.get('notes')
     question_type = data.get('question_type')
-    print(notes)
 
     notes_prompt = f"""Generate 5 {question_type} questions based on the notes I have provided below. Format it in JSON format like so: 
         {{
@@ -101,7 +102,6 @@ def get_question():
 
         I gave the structure for one question above but repeat for the other questions too in the same JSON object.
         Here are the notes: """
-
 
     final_prompt = notes_prompt + notes
     response = get_gpt_message(final_prompt)
