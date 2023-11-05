2d12f66b6edd47811bcaca72745330b4356e126c
Andrew Wang <awwang3@uci.edu>
2023-11-04 18:46:23 -0700

    auth.py

--- server/app/auth.py
+++ server/app/auth.py
@@ -1,19 +1,18 @@
-# from flask import request, Blueprint
-#
-# from . import google
-# from .model import db, User
-#
-# auth = Blueprint("auth", __name__)
-#
-#
-# @auth.route("/auth/login", methods=["GET", "POST"])
-# def login():
-#     return google.authorize_redirect(redirect_uri=)
-#     data = request.json
-#
-#     user_email = data.get('user_email')  # FRONTEND NEEDS TO PROVIDE
-#
-#     new_user = User(email=user_email)
-#     db.session.add(new_user)
-#     db.session.commit()
-#
+from flask import request, Blueprint
+
+from .model import db, User
+
+auth = Blueprint("auth", __name__)
+
+
+@auth.route("/auth/login", methods=["GET", "POST"])
+def login():
+    data = request.json
+
+    user_email = data.get('user_email')  # FRONTEND NEEDS TO PROVIDE
+
+    new_user = User(email=user_email)
+    db.session.add(new_user)
+    db.session.commit()
+
+
